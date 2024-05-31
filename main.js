7f665394c4ce64c4ba789466706ec077d0d62053
Sami B <167967927+SamiBoubguira@users.noreply.github.com>
2024-05-31 02:12:41 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-function updateDateTime() {
-    const now = new Date();
-    const currentDateTime = now.toLocaleString();
-    document.querySelector('#datetime').textContent = currentDateTime;
-}
-
-setInterval(updateDateTime, 1000);
